5693ea942824d7a939738e86ac0153ef3b54b9bb
Garg Arnav <q649286@africa.bmw.corp>
2025-01-12 10:16:08 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -182,6 +182,10 @@ export default function Form() {
         <h1>PI-64 Nikon Camera Register</h1>
       </div>
 
+      <div className="last-request">
+        <h3>Last User: {lastRequest || "Loading..."}</h3>
+      </div>
+
       {/* Buttons to switch between Request and Return modes
       <div className="button-container">
         <button className="btn" onClick={() => setShowWebcam(false)}>
